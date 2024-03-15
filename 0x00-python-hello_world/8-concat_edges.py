d005fbfdcbe48b15c6acefb41e5f478265cb57d7
narcissekinanswe <narcissekinanswe@gmail.com>
2024-03-15 04:10:17 -0700

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented programming"):].split()[0] + " " + str[str.find("object-oriented programming"):].split()[1] + " " + str[str.find("object-oriented programming"):].split()[2] + " " + str[str.find("object-oriented programming"):].split()[3] + " " + str[str.find("object-oriented programming"):].split()[4] + " " + str[str.find("object-oriented programming"):].split()[5] + ",")
+        language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
 
